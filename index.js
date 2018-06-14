3b591713fdd169f8a844b79114998f2b375505e9
Otabek Kamalov <otakam86@gmail.com>
2018-06-14 22:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
    return object;
 }
 function deleteFromObjectByKey(object, key){
-  delete object.key;
+ var newobj = delete object.key;
 }
