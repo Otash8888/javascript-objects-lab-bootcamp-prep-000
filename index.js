9c5ffdc5f802c7aaed39fefc392e62dc6a74916d
Otabek Kamalov <otakam86@gmail.com>
2018-06-14 22:42:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {
+  key:'value',
+  
+}
+function updateObjectWithKeyAndValue (object, key, value){
+  object[key]=value
+  var obj2=Object.assign({},object,{[key]:value})
+  
+  return obj2;
+}
